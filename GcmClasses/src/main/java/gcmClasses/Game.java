2e6bd00acfc4e8381702d0d0f109fd92bb32cebb
Taikunon <96242268+Taikunon@users.noreply.github.com>
2022-08-17 18:31:19 +0200

    updates

--- GcmClasses/src/main/java/gcmClasses/Game.java
+++ GcmClasses/src/main/java/gcmClasses/Game.java
@@ -14,96 +14,60 @@ import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @XmlRootElement
 
-public class Game  implements Serializable{
+public class Game implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
 
 	private int id;
 
 	private String gameTitle;
 
-
 	private LocalDate releaseDate;
 
-	// join table genre games
-
-	List<Genre> genres = new ArrayList<Genre>();
-
-	
-	
-
 	Set<Member> members = new HashSet<>();
-	
-	
-		
 
-    List<Tournament> tournaments = new ArrayList<Tournament>();
-	
-	
+	List<Tournament> tournaments = new ArrayList<Tournament>();
+
 	private String gameAdditionalNotes;
-
-
 
 	public Game() {
 		super();
 	}
 
-
-
-	public Game(String gameTitle, LocalDate releaseDate, List<Genre> genres, Set<Member> members,
-			List<Tournament> tournaments, String gameAdditionalNotes) {
+	public Game(String gameTitle, LocalDate releaseDate, Set<Member> members, List<Tournament> tournaments,
+			String gameAdditionalNotes) {
 		super();
 		this.gameTitle = gameTitle;
 		this.releaseDate = releaseDate;
-		this.genres = genres;
+
 		this.members = members;
 		this.tournaments = tournaments;
 		this.gameAdditionalNotes = gameAdditionalNotes;
 	}
 
-
-	@XmlElement(name="GameTitle")
+	@XmlElement(name = "GameTitle")
 	public String getGameTitle() {
 		return gameTitle;
 	}
-
-
 
 	public void setGameTitle(String gameTitle) {
 		this.gameTitle = gameTitle;
 	}
 
-
-	@XmlJavaTypeAdapter(value= LocalDateAdapter.class)
-	@XmlElement(name="ReleaseDate")
+	@XmlJavaTypeAdapter(value = LocalDateAdapter.class)
+	@XmlElement(name = "ReleaseDate")
 	public LocalDate getReleaseDate() {
 		return releaseDate;
 	}
-
-
 
 	public void setReleaseDate(LocalDate releaseDate) {
 		this.releaseDate = releaseDate;
 	}
 
 	@XmlTransient
-	public List<Genre> getGenres() {
-		return genres;
-	}
-
-
-
-	public void setGenres(List<Genre> genres) {
-		this.genres = genres;
-	}
-
-	@XmlTransient
 	public Set<Member> getMembers() {
 		return members;
 	}
-
-
 
 	public void setMembers(Set<Member> members) {
 		this.members = members;
@@ -114,52 +78,36 @@ public class Game  implements Serializable{
 		return tournaments;
 	}
 
-
-
 	public void setTournaments(List<Tournament> tournaments) {
 		this.tournaments = tournaments;
 	}
 
-
-	@XmlElement(name="GameAdditionalNotes")
+	@XmlElement(name = "GameAdditionalNotes")
 	public String getGameAdditionalNotes() {
 		return gameAdditionalNotes;
 	}
-
-
 
 	public void setGameAdditionalNotes(String gameAdditionalNotes) {
 		this.gameAdditionalNotes = gameAdditionalNotes;
 	}
 
-
-	@XmlElement(name="ID",required=true)
+	@XmlElement(name = "ID", required = true)
 	public int getId() {
 		return id;
 	}
-
-
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-
 	@Override
 	public String toString() {
-		return gameTitle ;
-				
+		return gameTitle;
+
 	}
-	
-	
 
-
-	
 }
